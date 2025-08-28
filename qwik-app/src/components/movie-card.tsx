bdd096c4bc85be6fb9ecf6719cedadb1170f65c8
evondev <trananhtuan400@gmail.com>
2025-08-28 22:02:02 +0700

    add zoom image

--- qwik-app/src/components/movie-card.tsx
+++ qwik-app/src/components/movie-card.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable qwik/jsx-img */
-import { component$ } from "@builder.io/qwik";
+import { $, component$, useSignal } from "@builder.io/qwik";
 
 interface MovieCardProps {
   poster_path: string;
@@ -7,18 +7,36 @@ interface MovieCardProps {
 }
 
 export const MovieCard = component$((props: MovieCardProps) => {
+  const isViewInModal = useSignal(false);
+  const handleViewImage = $(() => {
+    isViewInModal.value = true;
+  });
   return (
-    <div>
-      <div class="h-[480px] overflow-hidden rounded-xl">
-        <img
-          src={`https://image.tmdb.org/t/p/w500${props.poster_path}`}
-          alt={props.title}
-          class="size-full object-cover"
-          sizes="(max-width: 320px) 100vw, 320px"
-          loading="lazy"
-        />
+    <>
+      <div>
+        <div class="h-[480px] overflow-hidden rounded-xl">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${props.poster_path}`}
+            alt={props.title}
+            class="size-full cursor-zoom-in object-cover"
+            sizes="(max-width: 320px) 100vw, 320px"
+            loading="lazy"
+            onClick$={handleViewImage}
+          />
+        </div>
+        <h3 class="p-2 text-xl font-bold">{props.title}</h3>
       </div>
-      <h3 class="p-2 text-xl font-bold">{props.title}</h3>
-    </div>
+      {isViewInModal.value && (
+        <div class="modal fixed inset-0 z-20 flex items-center justify-center p-5">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${props.poster_path}`}
+            alt={props.title}
+            class="object-cover"
+            loading="lazy"
+            onClick$={() => (isViewInModal.value = false)}
+          />
+        </div>
+      )}
+    </>
   );
 });
